ac875090432d3c46621033374d2fa73d5cf5d39d
Myrionsc2 <marpht@hotmail.com>
2013-11-27 15:45:00 +0100

    Tilføjer stavekontrol funktion

---
+++ SpellCorrector.c
@@ -0,0 +1,59 @@
+
+ /*
+  * Dette program laver stavekontrol vha. Edit Distance metoden.
+  * Grundlag: http://norvig.com/spell-correct.html Python
+  * http://marcelotoledo.com/2007/08/10/how-to-write-a-spelling-corrector/
+  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Input/output til programmet:
+Fejlord
+korrektionsforslag
+
+
+Funktioner som skal være i programmet:
+
+Hver gang et ord køres igennem stavekontrol skal følgende ske:
+
+1: 	En funktion som sammenligner indtrykkede ord med alle ord i databasen for den del af tilstandsmaskinen.
+	Hvis intrykkede ord passer med et ord heri skal stavekontrollen ende. Hvis ikke fortsættes der.
+	
+2:	Edit1 funktioner udføres. Der kontroleres imens det udføres om fejlordet bliver til et ord i databasen.
+	selvom der kommer en positiv match skal programmet fortsætte med edit1 til alle muligheder er gennem kontroleret.
+	Hvis der er flere mulige korrektioner skal programmet se på hyppigheden af de mulige korrektioner, og foreslå
+	det ord som forekommer hyppigst. Bemærk at der skal FORESLÅS, ikke bare udføres, for hvis der er to ord der ligger
+	inden for edit1 er de højst sandsynligt så tæt på hindanden at 
+	
+	
+	
+Edit1			// En samling af funktioner som redigerer ordet en gang
+  Indsættelse	//Indsætter et bogstav i fejlordet og checker.
+  Slettelse		//Sletter et bogstav
+  Erstatning	//Erstatter et bogstav med et andet
+  Ombytning		//Bytter rundt på to tilstødende bogstaver
+
+edit2			// Hvis edit1 skal udføres to gange for at et ord bliver korrekt.
+
+*/
+
+int main() {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+}
+
+
+
+
