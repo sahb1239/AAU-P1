73d9e1cc10f8657638c6500c6f5de66cf5e994df
Edelskjold <fan2008@live.dk>
2013-12-03 13:19:32 +0100

    Return / void

--- users.c
+++ users.c
@@ -30,9 +30,6 @@ void printUsers(const USERS users[], int len) {
 
 void deleteUsers(const USERS users[], int len, char inputname[]){
    	FILE *pFile1 = fopen(FILE_USERS, "w");
-	if (pFile1 == NULL) {
-		return -1;
-	}
     
     for (i = 0; i<len; i++) {
     	if (strcmp(users[i].name, inputname) != 0){
