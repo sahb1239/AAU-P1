e5948c95e0612a5aa21db5cabb371b53f751d197
Edelskjold <fan2008@live.dk>
2013-12-03 13:05:29 +0100

    More \t

--- users.c
+++ users.c
@@ -24,6 +24,6 @@ void printUsers(const USERS users[], int len) {
     int i;
     	printf("Prioritet:\tBruger:\n");
     for (i = 0; i < len; i++) {
-    	printf("%d\t%s\n", users[i].priority, users[i].name);
+    	printf("%d\t\t%s\n", users[i].priority, users[i].name);
 	}
 }
