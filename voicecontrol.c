d7a83f6f6acb75c0ae235d6ad6a6857ff516b04e
sahb1239 <sahb1239@gmail.com>
2013-12-04 15:38:16 +0100

    Prøv nu

--- voicecontrol.c
+++ voicecontrol.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
 			printf("START");
 			for (i = 0; i < len; i++) {
 					int tmp2;
-					char *tmp = correct(out[i], &tmp2);
+					free(correct(out[i], &tmp2));
 			}
 
     		/* Free splitString array */
