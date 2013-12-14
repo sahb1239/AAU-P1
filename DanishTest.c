07253ff67b3216c8157fbd63f1d91ed06b1105f4
sahb1239 <sahb1239@gmail.com>
2013-12-14 12:02:38 +0100

    Opdateret DanishTest

--- DanishTest.c
+++ DanishTest.c
@@ -4,15 +4,15 @@
 int main(void) {
 	FILE *file = fopen("dtest", "r");
 	
-	char chr[10], chr2[10], chr3[] = "æ";
+	char chr[10], chr2[10];
 	
 	printf("Indtast input => ");
 	fscanf(file, "%s", chr);
 	scanf("%s", chr2);
 	
-	printf("Fil: %s\nKonsol: %s\nVariabel: %s\n", chr, chr2, chr3);
+	printf("Fil: %s\nKonsol: %s\n", chr, chr2);
 	
-	if (chr[0] == chr2[0] && chr[0] == chr3[0])
+	if (chr[0] == chr2[0])
 		printf("ER LIG\n");
 	else
 		printf("ER IKKE LIG\n");
